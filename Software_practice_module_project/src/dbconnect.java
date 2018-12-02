4e79221f55f39439e1cde9f229e2f57709c1c7bd
manue <manue@LAPTOP-2KRUJLRB.fritz.box>
2018-12-02 18:11:30 +0100

    DBconnect

--- Software_practice_module_project/src/dbconnect.java
+++ Software_practice_module_project/src/dbconnect.java
@@ -4,9 +4,10 @@ public class dbconnect {
 
 	public static void main(String[] args) throws SQLException {
 
-		Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "1234");
+		Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "qwerty");
 		if (con != null) {
 			System.out.println("DATABASE CONNECTED..");
 		}
 	}
 }
+
