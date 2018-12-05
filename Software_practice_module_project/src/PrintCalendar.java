d6c2ae32e085014d2b1ad1174c797b8c38a94cc5
manue <manue@LAPTOP-2KRUJLRB>
2018-12-05 14:55:09 +0100

    Calendar

--- Software_practice_module_project/src/PrintCalendar.java
+++ Software_practice_module_project/src/PrintCalendar.java
@@ -20,18 +20,12 @@ import java.util.Scanner;
            printMonth(year, month);
 
    }
-    /** Print the calendar for a month in a year */
 
      static void printMonth(int year, int month) {
-
-     //Print the headings of the calendar
       printMonthTitle(year, month);
-
-     //Print the body of the calendar
       printMonthBody(year, month);
     }
 
-    /** Print the month title, e.g., May, 1999 */
 
     static void printMonthTitle(int year, int month) {
 
@@ -41,7 +35,6 @@ import java.util.Scanner;
 
     }
 
-    /** Get the English name for the month */
     static String getMonthName(int month) {
       String monthName = null;
       switch (month) {
@@ -61,16 +54,12 @@ import java.util.Scanner;
       return monthName;
     }
 
-    /** Print month body */
     static void printMonthBody(int year, int month) {
 
-      // Get start day of the week for the first date in the month
       int startDay = getStartDay(year, month);
 
-      // Get number of days in the month
       int numberOfDaysInMonth = getNumberOfDaysInMonth(year, month);
 
-      // Pad space before the first day of the month
       int i = 0;
       for (i = 0; i < startDay; i++)
         System.out.print("    ");
@@ -85,38 +74,24 @@ import java.util.Scanner;
       System.out.println();
     }
 
-    /** Get the start day of the first day in a month */
-
    static int getStartDay(int year, int month) {
-
-      //Get total number of days since 1/1/1800
       int startDay1800 = 3;
       int totalNumberOfDays = getTotalNumberOfDays(year, month);
-
-      //Return the start day
       return (totalNumberOfDays + startDay1800) % 7;
     }
 
-    /** Get the total number of days since January 1, 1800 */
-
     static int getTotalNumberOfDays(int year, int month) {
      int total = 0;
-
-     //Get the total days from 1800 to year - 1
      for (int i = 1800; i < year; i++)
      if (isLeapYear(i))
         total = total + 366;
       else
         total = total + 365;
-
-      //Add days from January to the month prior to the calendar month
       for (int i = 1; i < month; i++)
         total = total + getNumberOfDaysInMonth(year, i);
 
       return total;
     }
-
-    /** Get the number of days in a month */
 
     static int getNumberOfDaysInMonth(int year, int month) {
       if (month == 1 || month == 3 || month == 5 || month == 7 ||
@@ -128,10 +103,9 @@ import java.util.Scanner;
 
       if (month == 2) return isLeapYear(year) ? 29 : 28;
 
-      return 0; // If month is incorrect
+      return 0;
     }
 
-    /** Determine if it is a leap year */
     static boolean isLeapYear(int year) {
       return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
