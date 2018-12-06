422dbb3d9a58eccec2397d42c1af8220a1013420
ignacio zaldivar <gimnacio014@gmail.com>
2018-12-06 14:22:03 +0100

    print calendar now shows the match  place

--- Software_practice_module_project/src/PrintCalendar.java
+++ Software_practice_module_project/src/PrintCalendar.java
@@ -1,112 +1,153 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
-   public class PrintCalendar {
+public class PrintCalendar {
 
-     public static void main(String[] args) {
-     Scanner scan = new Scanner (System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-     //Prompt the user to enter year
-     System.out.print("Enter full year (e.g., 2001): ");
-     int year = scan.nextInt();
+		// Prompt the user to enter year
+		System.out.print("Enter full year (e.g., 2001): ");
+		int year = scan.nextInt();
 
-     // Prompt the user to enter month
-     System.out.print("Enter month in number between 1 and 12: ");
-     int month = scan.nextInt();
+		// Prompt the user to enter month
+		System.out.print("Enter month in number between 1 and 12: ");
+		int month = scan.nextInt();
 
-     // Print calendar for the month of the year
-      if (month < 1 || month > 12 || year < 1980)
-       System.out.println("Wrong input!");
-       else
-           printMonth(year, month);
+		// Print calendar for the month of the year
+		if (month < 1 || month > 12 || year < 1980)
+			System.out.println("Wrong input!");
+		else
+			printMonth(year, month);
+		try {
+			Connection dbConnection = dbconnect.dbconnect();
+			String query = queries.getMatchQuery();
+			PreparedStatement statement = dbConnection.prepareStatement(query);
+			statement = dbConnection.prepareStatement(query);
+			statement.setInt(1, month);
+			statement.setInt(2, year);
+			ResultSet resultSet = statement.executeQuery();
+			while (resultSet.next()) {
+				String element1 = resultSet.getString("match_place");
+				System.out.println(element1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-   }
+	static void printMonth(int year, int month) {
+		printMonthTitle(year, month);
+		printMonthBody(year, month);
+	}
 
-     static void printMonth(int year, int month) {
-      printMonthTitle(year, month);
-      printMonthBody(year, month);
-    }
+	static void printMonthTitle(int year, int month) {
 
+		System.out.println("         " + getMonthName(month) + " " + year);
+		System.out.println("æœ«æœ«æœ«æœ«æœ«æœ«æœ«æœ«æœ«æœ«æœ«æœ«æœ«æœ«ï¿½");
+		System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
 
-    static void printMonthTitle(int year, int month) {
+	}
 
-    System.out.println("         " + getMonthName(month) + " " + year);
-    System.out.println("–––––––––––––––––––––––––––––");
-    System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
+	static String getMonthName(int month) {
+		String monthName = null;
+		switch (month) {
+		case 1:
+			monthName = "January";
+			break;
+		case 2:
+			monthName = "February";
+			break;
+		case 3:
+			monthName = "March";
+			break;
+		case 4:
+			monthName = "April";
+			break;
+		case 5:
+			monthName = "May";
+			break;
+		case 6:
+			monthName = "June";
+			break;
+		case 7:
+			monthName = "July";
+			break;
+		case 8:
+			monthName = "August";
+			break;
+		case 9:
+			monthName = "September";
+			break;
+		case 10:
+			monthName = "October";
+			break;
+		case 11:
+			monthName = "November";
+			break;
+		case 12:
+			monthName = "December";
+		}
+		return monthName;
+	}
 
-    }
+	static void printMonthBody(int year, int month) {
 
-    static String getMonthName(int month) {
-      String monthName = null;
-      switch (month) {
-        case 1: monthName = "January"; break;
-        case 2: monthName = "February"; break;
-        case 3: monthName = "March"; break;
-        case 4: monthName = "April"; break;
-        case 5: monthName = "May"; break;
-        case 6: monthName = "June"; break;
-        case 7: monthName = "July"; break;
-        case 8: monthName = "August"; break;
-        case 9: monthName = "September"; break;
-        case 10: monthName = "October"; break;
-        case 11: monthName = "November"; break;
-        case 12: monthName = "December";
-      }
-      return monthName;
-    }
+		int startDay = getStartDay(year, month);
 
-    static void printMonthBody(int year, int month) {
+		int numberOfDaysInMonth = getNumberOfDaysInMonth(year, month);
 
-      int startDay = getStartDay(year, month);
+		int i = 0;
+		for (i = 0; i < startDay; i++)
+			System.out.print("    ");
+		for (i = 1; i <= numberOfDaysInMonth; i++) {
+			if (i < 10)
+				System.out.print("   " + i);
+			else
+				System.out.print("  " + i);
+			if ((i + startDay) % 7 == 0)
+				System.out.println();
+		}
+		System.out.println();
+	}
 
-      int numberOfDaysInMonth = getNumberOfDaysInMonth(year, month);
+	static int getStartDay(int year, int month) {
+		int startDay1800 = 3;
+		int totalNumberOfDays = getTotalNumberOfDays(year, month);
+		return (totalNumberOfDays + startDay1800) % 7;
+	}
 
-      int i = 0;
-      for (i = 0; i < startDay; i++)
-        System.out.print("    ");
-      for (i = 1; i <= numberOfDaysInMonth; i++) {
-        if (i < 10)
-          System.out.print("   " + i);
-        else
-          System.out.print("  " + i);
-        if ((i + startDay) % 7 == 0)
-          System.out.println();
-      }
-      System.out.println();
-    }
+	static int getTotalNumberOfDays(int year, int month) {
+		int total = 0;
+		for (int i = 1800; i < year; i++)
+			if (isLeapYear(i))
+				total = total + 366;
+			else
+				total = total + 365;
+		for (int i = 1; i < month; i++)
+			total = total + getNumberOfDaysInMonth(year, i);
 
-   static int getStartDay(int year, int month) {
-      int startDay1800 = 3;
-      int totalNumberOfDays = getTotalNumberOfDays(year, month);
-      return (totalNumberOfDays + startDay1800) % 7;
-    }
+		return total;
+	}
 
-    static int getTotalNumberOfDays(int year, int month) {
-     int total = 0;
-     for (int i = 1800; i < year; i++)
-     if (isLeapYear(i))
-        total = total + 366;
-      else
-        total = total + 365;
-      for (int i = 1; i < month; i++)
-        total = total + getNumberOfDaysInMonth(year, i);
+	static int getNumberOfDaysInMonth(int year, int month) {
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+			return 31;
 
-      return total;
-    }
+		if (month == 4 || month == 6 || month == 9 || month == 11)
+			return 30;
 
-    static int getNumberOfDaysInMonth(int year, int month) {
-      if (month == 1 || month == 3 || month == 5 || month == 7 ||
-        month == 8 || month == 10 || month == 12)
-        return 31;
+		if (month == 2)
+			return isLeapYear(year) ? 29 : 28;
 
-      if (month == 4 || month == 6 || month == 9 || month == 11)
-        return 30;
+		return 0;
+	}
 
-      if (month == 2) return isLeapYear(year) ? 29 : 28;
-
-      return 0;
-    }
-
-    static boolean isLeapYear(int year) {
-      return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
-    }
+	static boolean isLeapYear(int year) {
+		return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+	}
 }
