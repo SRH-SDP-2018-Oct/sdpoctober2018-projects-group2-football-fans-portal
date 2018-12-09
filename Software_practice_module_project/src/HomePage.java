ba6c1c12c6ebc8449b071e087ed95629de51dc2c
manue <manue@LAPTOP-2KRUJLRB.fritz.box>
2018-12-09 10:31:19 +0100

    sin comentario

--- Software_practice_module_project/src/HomePage.java
+++ Software_practice_module_project/src/HomePage.java
@@ -2,11 +2,8 @@ import java.util.Scanner;
 public class HomePage {
     
     public static void main(String [] args){
-    
-    	//dbconnect.dbconnect();
     	
         Scanner scanner = new Scanner(System.in);
-        // Display the menu
         System.out.println("Welcome to Football Fans Portal");
         System.out.println("Selection of the options");
         System.out.println("1\t Login");
@@ -17,10 +14,8 @@ public class HomePage {
 
         System.out.println("Please enter your choice:");
         
-        //Get user's choice
         int choice=scanner.nextInt();
          
-        //Display the title of the chosen module
         switch (choice) {
             case 1: log_in.method1();
 	                break;
@@ -35,6 +30,6 @@ public class HomePage {
             default: System.out.println("Invalid choice");
             
             scanner.close();
-        }//end of switch
-    }//end of the main method
-}//end of class
+        }
+    }
+}
